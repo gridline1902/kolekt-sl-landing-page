fb5a6c89949f9b0e43b4dd660dd54716e55f6809
gridline1902 <michaeljrfreeman@gmail.com>
2024-09-05 17:24:18 +0000

    update.....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import NavBar from "@/components/NavBar";
 import HeroSection from "@/components/HeroSection";
 import PartnersSection from "@/components/PartnersSection/PartnersSection";
 import KolektSolutionsSection from "@/components/KolektSolutionsSection/KolektSolutionsSection";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <PartnersSection />
       <KolektSolutionsSection />
+      <Footer />
     </main>
   );
 }
