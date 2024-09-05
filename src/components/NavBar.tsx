e659ed48e553521500663a534fb252cf9afa8432
gridline1902 <michaeljrfreeman@gmail.com>
2024-09-05 17:23:57 +0000

    update.....

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,22 @@
 
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { BiHeadphone } from "react-icons/bi";
+import Button from "./Button";
+
 const NavBar = () => {
     return (
-        <nav className="z-20 top-0 start-0 fixed w-full h-16 bg-slate-50">
-           
+        <nav className="z-20 top-0 start-0 sticky w-full h-16 bg-slate-50">
+            <div className="flex items-center justify-between h-full px-4">
+                <Image src={'/Kolekt-v3.0.png'} alt="kolekt-logo" width={100} height={100} />
+                <div className="flex items-center gap-4">
+                    <Link href={'/'}><span className="flex items-center"><BiHeadphone className="text-base" /><h6 className="text-sm">Contact</h6></span></Link>
+                    <Button className="h-8 px-2">Create a free account</Button>
+                </div>
+            </div>
+
         </nav>
     );
 }
