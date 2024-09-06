3da325319c5dbe6cc44c4d19199967ea1b88122f
gridline1902 <michaeljrfreeman@gmail.com>
2024-09-06 16:13:06 +0000

    layout update....

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kolekt by Monime",
-  description: "Kolekt is powered by Monime Limited, a company registered in Sierra Leone with registered company number SL140722MONIM14909 and its registered office at 5 Hillside Bypass Road, Freetown, Sierra Leone. Monime Limited is authorised by the Bank of Sierra Leone under the Sandbox program for the provision of payment services. All trademarks and brand names belong to their respective owners. Use of these trademarks and brand names do not represent endorsement by or association with our payment services.",
+  description: "A simple way to accept payments at scale, with ZERO costs. Delight customers by allowing frontline staff to confirm payments via WhatsApp, instantly.",
 };
 
 export default function RootLayout({
