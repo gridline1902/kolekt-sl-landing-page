bb1e7360a5923e6b3d89de5aa9af2bd7d745c8f5
gridline1902 <michaeljrfreeman@gmail.com>
2024-09-10 14:57:33 +0000

    update.....

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,14 +7,14 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 const HeroSection = () => {
-
     const containerRef = useRef(null);
     const h1Ref = useRef<HTMLHeadingElement>(null);
     const h6Ref = useRef<HTMLHeadingElement>(null);
+    const imageRef = useRef<HTMLDivElement>(null);
 
     useGSAP(() => {
         const timeLine = gsap.timeline();
-        if (h1Ref.current && h6Ref.current) {
+        if (h1Ref.current && h6Ref.current && imageRef.current) {
             timeLine
                 .from([h1Ref.current.children, h6Ref.current.children], {
                     xPercent: gsap.utils.wrap([100, -100]),
@@ -23,30 +23,30 @@ const HeroSection = () => {
                     ease: "circ.inOut",
                     duration: 1,
                 })
-                .to([h1Ref.current.children, h6Ref.current.children], {
+                .from([imageRef.current], {
+                    yPercent: gsap.utils.wrap([100, -100]),
+                    stagger: 0.4,
+                    opacity: 0,
+                    ease: "circ.inOut",
+                    duration: 1,
+                })
+                .to([h1Ref.current.children, h6Ref.current.children, imageRef.current.children], {
                     opacity: 1,
                     duration: 2,
                     onComplete: () => {
                         timeLine.kill();
                     }
                 })
-                .to([h1Ref.current.children, h6Ref.current.children], {
-                    opacity: 0,
-                    yPercent: gsap.utils.wrap([-100, 100]),
-                    duration: 1,
-                    ease: "none",
-                }, ">1.5");
         }
 
     }, { scope: containerRef });
 
-
     return (
-        <section ref={containerRef} className="min-h-screen bg-[#072048] overflow-hidden relative">
-            <div className="flex flex-col lg:grid grid-cols-2 relative z-10 min-h-screen lg:items-center lg:justify-center">
-                <div className="flex flex-col gap-10 lg:items-end lg:justify-center">
+        <section ref={containerRef} className="h-auto bg-[#072048] overflow-hidden relative">
+            <div className="flex flex-col pt-12 lg:grid lg:grid-cols-2 relative z-10 min-h-screen lg:items-center lg:justify-center">
+                <div className="flex flex-col gap-10 items-center justify-center lg:items-end lg:justify-center text-center lg:text-left">
                     <div className="flex flex-col gap-8">
-                        <h1 ref={h1Ref} className="text-slate-50 text-6xl tracking-wide font-extrabold max-w-sm text-left overflow-hidden">
+                        <h1 ref={h1Ref} className="text-slate-50 text-[3.3rem] leading-[1.2] tracking-wide font-extrabold max-w-sm overflow-hidden lg:text-6xl">
                             <div>
                                 <div>Get paid,</div>
                             </div>
@@ -54,7 +54,7 @@ const HeroSection = () => {
                                 <div>quickly</div>
                             </div>
                         </h1>
-                        <h6 ref={h6Ref} className="text-slate-50 text-2xl text-left max-w-lg overflow-hidden">
+                        <h6 ref={h6Ref} className="text-slate-50 text-lg max-w-lg overflow-hidden text-wrap lg:text-2xl">
                             <div>
                                 <div>Receive, confirm, and instantly settle payments directly to your mobile wallet -</div>
                             </div>
@@ -62,16 +62,19 @@ const HeroSection = () => {
                                 <div><span className="font-bold">completely free of charge.</span></div>
                             </div>
                         </h6>
-                        <Button className="h-11 w-[40%]">Create a free account</Button>
+                        <div>
+                            <Button className="h-auto w-auto py-3 px-3">Create a free account</Button>
+                        </div>
                     </div>
                 </div>
-                <Image
-                    src="/Kolekt-Home-v2-2.png"
-                    className="lg:absolute right-40 scale-75"
-                    alt="kolekt-payments"
-                    width={500}
-                    height={400}
-                />
+                <div ref={imageRef} className="relative scale-75 lg:absolute lg:right-40 lg:scale-75 lg:h-full">
+                    <Image
+                        src="/Kolekt-Home-v2-2.png"
+                        alt="kolekt-payments"
+                        width={500}
+                        height={400}
+                    />
+                </div>
             </div>
         </section>
     );
