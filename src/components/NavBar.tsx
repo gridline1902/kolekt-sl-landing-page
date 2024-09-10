c3b4e15203790aed960ab2cdfb964b0ea5d588d9
gridline1902 <michaeljrfreeman@gmail.com>
2024-09-10 14:57:39 +0000

    update.....

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,12 +7,9 @@ import { GiHeadphones } from "react-icons/gi";
 
 const NavBar = () => {
     return (
-        <nav className="z-20 top-0 start-0 sticky w-full h-16 bg-slate-50 flex items-center justify-center"> {/* Center everything inside nav */}
-            <div className="flex items-center justify-between w-full max-w-6xl px-4"> {/* Wrapper for spacing */}
-                {/* Logo */}
-                <Image src={'/Kolekt-v3.0.png'} alt="kolekt-logo" width={100} height={100} />
-
-                {/* Centered items */}
+        <nav className="z-20 top-0 start-0 sticky w-full h-24 bg-slate-50 flex items-center justify-center">
+            <div className="sm: hidden lg:flex lg:items-center lg:justify-between lg:w-full lg:max-w-6xl lg:px-4"> 
+                <Image src={'/Kolekt-v3.0.png'} alt="kolekt-logo" width={100} height={100} className="w-auto h-auto" />
                 <div className="flex items-center gap-x-6">
                     <Link href={'/'}>
                         <span className="flex items-center gap-1">
